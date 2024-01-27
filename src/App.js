ab7894139992e359aa810374f9c4289125cd1489
Messam-Naqvi <messamnaqvi22@gmail.com>
2024-01-27 18:02:06 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Note" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <Home />
     </>
   );
 }
